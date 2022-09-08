5e555b737c5ad4bfeb1993ab09cee558068f377a
vchou2324 <79931009+vchou2324@users.noreply.github.com>
2022-09-08 16:14:07 -0700

    Update Classroom.java

--- Classroom.java
+++ Classroom.java
@@ -231,6 +231,11 @@ public class Classroom extends World
         AbhinithiJanardhanan abhinithijanardhanan = new AbhinithiJanardhanan();
         addObject(abhinithijanardhanan, 2, 4);
         abhinithijanardhanan.assignSeat();  
+        
+        ViswaChoudhary viswachoudhary = new ViswaChoudhary("Viswa", "Choudhary",2,4); //added this to call the method in ViswaChoudhary.java
+        addObject(viswachoudhary,2,4); //added this to add an object and give it a row and column number
+        viswachoudhary.assignSeat(); // assigned a seat pertained to the object I created        
+        
 
     }
 
